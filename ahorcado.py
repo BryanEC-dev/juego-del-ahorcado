1e9a1430ba6b094ea265cc3127f711d2c29a1d21
Bryan <bryan_desarrollo_ec@outlook.com>
2020-10-23 14:20:17 -0500

    Las partes del juego se encuentran funcionales

--- ahorcado.py
+++ ahorcado.py
@@ -1,8 +1,16 @@
-import random
+
 import welcome
 import repository.words as words
 import utilities.secuence as secuence
 
+
+numberHelp = 0
+tries = 0
+lettersEntered = []
+repeated_letter = False
+one_letter = ''
+word = ''
+hidden_word = ''
 # get the sequence of images of the game
 IMAGES = secuence.images()
 
@@ -14,29 +22,129 @@ def display_board(hidden_word, tries):
     print(IMAGES[tries])
     print('')
     print(hidden_word)
-    print('----+ ----+ ----+ ----+ ----+ ----+ ----+ ----+')
+    print('----+ ----+ ----+ ----+ ----+ ----+ ----+ ----+ ----+ ----+ ----+')
 
-
-def random_word():
-    length = len(WORDS) - 1
-    idx = random.randint(0, length)
-    return WORDS[idx]
 
 
 def run():
-    word = random_word()
+    global word
+    global hidden_word
+    """ get a random word """
+    #randomWord = secuence.randomWord(WORDS)
+    word = 'lavaplatos' #randomWord[1]
     hidden_word = ['-'] * len(word)
-    tries = 0
-    letters = []
-    repeated_letter = False 
-    one_letter =''
+
 
     while True:
         display_board(hidden_word, tries)
-        current_letter = str(input('Ingresa una letra: '))
+        option = int(input("""
+            1.Obtener una pista
+            2.Ingresar una letra o palabra   
+        """))
 
-        """ validar si ingreso la palabra correcta """
-        if len(current_letter) > 1:
+        if option == 1:
+
+            global numberHelp
+            if numberHelp == 3:
+                print("ya no tienes mas pistas")
+                enter = input("Presiona enter para continuar....")
+            else:
+                idx = numberHelp + 2
+                print(randomWord[idx])
+                enter = input("Presiona enter para continuar....")
+                numberHelp +=1
+                continue
+
+        elif option == 2:
+            enterLetter()
+        else:
+            print('La opción ingresada es incorrecta')
+
+        
+def enterLetter():
+    letters = input("Ingrese una letra o una palabra: ")
+
+    #TODO validar si la palabra ingresada no cuntiene caracteres no permitidos 
+
+    if len(letters) > 1:
+        won = verifyWord(letters)
+        if won == True:
+            pass
+            # finde del juego
+        else:
+            pass
+            #aumenta el contador de errores
+    else:
+        verifyLetter(letters)
+        pass
+
+
+
+def verifyWord(letters):
+    if letters == words:
+        return True
+    else:
+        return False
+
+
+def verifyLetter(letter):
+    global word
+    # check if the letter has already been entered
+    if len(lettersEntered) == 0:
+        lettersEntered.append(letter)
+    else:
+        if letter in lettersEntered:
+            print("La letra ya fue ingresada.")
+            # debería detenerse el flujo
+        else:
+            lettersEntered.append(letter)
+    # validate if the letter is in the word
+    print('la palabara es',word)
+    if (not letter in word ):
+        global tries
+        tries +=1
+    else:
+        replaceLetter(letter)
+        pass
+
+
+def replaceLetter(letter):
+    print('enntrando a función')
+    global word
+    global hidden_word
+    wordTemporal = []
+
+    for x in word:
+        if x == letter:
+            wordTemporal.append(letter)
+        else:
+            wordTemporal.append('-')
+    
+    hidden_word = wordTemporal
+            
+
+
+def gameOver():
+    if tries == 7:
+         display_board(hidden_word, tries)
+         print('')
+         print('¡ Perdiste!, la palabra correcta era {}'.format(word))
+         
+
+               
+
+def iterable():
+    lista = ['a', 'b' , 'c' , 'd']
+
+    for iter in lista:
+        print(iter)
+
+
+def core():
+    """ current_letter = str(input('Ingresa una letra: '))
+
+        # comentario validar si ingreso la palabra correcta
+       if len(current_letter) > 1:
             print('ingresaste la palabra: {}'.format(current_letter))
             if current_letter == word:
                print('')
@@ -56,8 +164,8 @@ def run():
             one_letter = current_letter[0]
 
         print(one_letter)
-        """ one_letter = current_letter[0] """
-        """ Validar si la letra ya ha sido ingreada """
+        # comentario  one_letter = current_letter[0]
+         # comentario Validar si la letra ya ha sido ingreada
         for l in letters:
             print('{} no es igual a {}'.format(one_letter, letters))
             if one_letter == l:
@@ -65,27 +173,27 @@ def run():
                repeated_letter = True
                break
 
-        if repeated_letter == True :
+        if repeated_letter == True:
             repeated_letter = False
             continue
 
-        print('continue con el flujo')  
+        print('continue con el flujo')
         letter_indexes = []
-        for idx  in range(len(word)):
+        for idx in range(len(word)):
             if word[idx] == current_letter:
                 letter_indexes.append(idx)
 
         if len(letter_indexes) == 0:
-            tries +=1
+            tries += 1
 
             if tries == 7:
-                display_board(hidden_word,tries)
+                display_board(hidden_word, tries)
                 print('')
                 print('¡ Perdiste!, la palabra correcta era {}'.format(word))
                 break
         else:
             for idx in letter_indexes:
-                hidden_word[idx] = current_letter    
+                hidden_word[idx] = current_letter
             letter_indexes = []
 
         try:
@@ -95,18 +203,13 @@ def run():
             print('Felicidades')
             print('¡Felicitaciones! ganaste, la palabra es: {}'.format(word))
             break
-        
-        letters.append(one_letter)
 
-def iterable():
-    lista = ['a', 'b' , 'c' , 'd']
-
-    for iter in lista:
-        print(iter)
+        letters.append(one_letter) """
 
 if __name__ == '__main__':
     welcome.welcome()
-    test()
+    run()
+    
 
 
     
--- utilities/secuence.py
+++ utilities/secuence.py
@@ -1,3 +1,4 @@
+import random
 IMAGES = ['''
 
     +---+
@@ -67,3 +68,16 @@ IMAGES = ['''
 
 def images():
     return IMAGES
+
+
+""" get a random word """
+
+
+def randomWord(WORDS):
+    length = len(WORDS) - 1
+    idx = random.randint(0, length)
+    return WORDS[idx]
+
+
+def help(randomWord, numberHelp):
+    return
